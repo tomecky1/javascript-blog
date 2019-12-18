545b6562ea6a463427913c12b74f605df6a581f6
Tomek Piotrowski <tpiotrowski007@gmail.com>
2019-12-18 23:01:01 +0100

    Add function to generate title links

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+{
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -29,3 +30,23 @@ const links = document.querySelectorAll('.titles a');
 for (const link of links) {
     link.addEventListener('click', titleClickHandler);
     }
+const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+function generateTitleLinks(){
+  /* remove contents of titleList */
+
+  /* for each article */
+
+  /* get the article id */
+
+  /* find the title element */
+
+  /* get the title from the title element */
+
+  /* create HTML of the link */
+
+  /* insert link into titleList */
+}
+generateTitleLinks();
+}    
