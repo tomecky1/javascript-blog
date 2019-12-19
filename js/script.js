d68a388a818896463519d7f8ee468f80f7d53da9
Tomek Piotrowski <tpiotrowski007@gmail.com>
2019-12-19 23:26:12 +0100

    Add function generate title links

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ const titleClickHandler = function(event){
       targetArticle.classList.add('active');
     }
 const links = document.querySelectorAll('.titles a');
+console.log('link: ', links);
 for (const link of links) {
     link.addEventListener('click', titleClickHandler);
     }
@@ -34,19 +35,25 @@ const optArticleSelector = '.post',
       optTitleSelector = '.post-title',
       optTitleListSelector = '.titles';
 function generateTitleLinks(){
-  /* remove contents of titleList */
-
-  /* for each article */
-
-  /* get the article id */
-
-  /* find the title element */
-
-  /* get the title from the title element */
-
-  /* create HTML of the link */
-
-  /* insert link into titleList */
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  let html = '';
+  /* [DONE] for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  for (const article of articles) {
+  /* [DONE] get the article id */
+  const articleId = article.getAttribute('id');
+  /* [DONE] find the title element */
+  /* [DONE] get the title from the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  /* [DONE] create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log('linkHTML: ', linkHTML);
+  /* [IN PROGRESS] insert link into html variable */
+  html = html + linkHTML;
+   
+  }
+  titleList.innerHTML = html;
 }
 generateTitleLinks();
 }    
